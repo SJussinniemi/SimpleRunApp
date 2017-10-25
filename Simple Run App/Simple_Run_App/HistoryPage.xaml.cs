b7fdf92e53b38a01a6113a80b9e2467c3b2bd05e
SJussinniemi <S.Jussinniemi@gmail.com>
2017-10-25 12:24:15 +0300

    Template for you adrian

--- Simple Run App/Simple_Run_App/HistoryPage.xaml.cs
+++ Simple Run App/Simple_Run_App/HistoryPage.xaml.cs
@@ -12,14 +12,11 @@ namespace Simple_Run_App
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HistoryPage : ContentPage
     {
-        int aux;
-        int i;
+
         public HistoryPage()
         {
-            
             InitializeComponent();
             //ExerciseView.IsEnabled = false;
-            aux = 0;
         }
 
         protected override async void OnAppearing()
@@ -30,50 +27,37 @@ namespace Simple_Run_App
             show = await db.GetItemsAsync();
 
             try
-            {
-                NumElements.Text = "Count of rows in list: " + show.Count().ToString();
-                //StackLayout s1 = new StackLayout();
-                if (aux == show.Count())
+            {  
+                for (int i = 0; i < show.Count(); i++)
                 {
-                    InitializeComponent();
-                    aux = 0;
+                    historyPage.Children.Add(new Label {FontSize = 16, Text = " Exercise number: " + show[i].ID + "\n Date: " + show[i].DATETIME.ToString() + "\n " + show[i].DURATION + "\n " + show[i].DISTANCE + "\n Average Speed: " + show[i].AVGSPEED + " Km/h" });
+                    historyPage.Children.Add(new Button { Text = "Delete",
+                        Command = new Command(async () => {
+
+                            bool answer = await DisplayAlert("Notice!", "Are you sure you want to delete this exercise?", "Yes", "No");
+                            if (answer)
+                            {
+
+                            }
+
+                        })
+                    });
                 }
-                else
-                {
-                    for (i = aux; i < show.Count(); i++)// Creatting tje Labels
-                    {
-                        historyPage.Children.Add(new Label { Text = "Exercise number: " + show[i].ID + " - Delete Code: " + i + "\n Date: " + show[i].DATETIME.ToString() + "\n " + show[i].DURATION + "\n " + show[i].DISTANCE + "\n Average Speed: " + show[i].AVGSPEED + " Km/h" });
-                        historyPage.Children.Add(new Button { Text = "Delete" });
-                    }
-                    Content = new ScrollView { Content = historyPage };
-                    //Content = historyPage;
-                    aux = i;// If the user add a new Exercise history, the next time when i press this btn the for starts in the las position and prints only the last additions
-                }
+
+                Content = new ScrollView { Content = historyPage };
+                
             }
             catch (Exception ex)
             {
-                NumElements.Text = ex.ToString();
+                debug.Text = ex.ToString();
             }
         }
 
-        private async void DelateExer(object sender, EventArgs e)
+        protected override void OnDisappearing()
         {
-
-            int IDExer = int.Parse(entryID.Text); ; //The Exercise ID that the user wants to delate
-
-            var db = App.database;
-            var list = new List<ExerciseTable>();
-            list = await db.GetItemsAsync();
-            if (IDExer < 0 || IDExer >= list.Count())
-            {
-                await DisplayAlert("Sorry!!", "The Delete Code doesn't Exist, please put another one", "ok");
-            }
-            else
-            {
-                aux = 0;
-                await db.DeleteItemAsync(list[IDExer]); //Delate this list frome the dataBase. 
-                InitializeComponent();
-            }
+            base.OnDisappearing();
+            historyPage.Children.Clear();
         }
+
     }
 }
